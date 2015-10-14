f616773efa5125a662d039b6e0fc704a86d9514d
Vlad-Stefan Harbuz <vlad@vladh.net>
2015-10-15 00:23:01 +0100

    thursday → tday, remove *.pyc

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import thursday.music
-import thursday.scores
+import tday.music
+import tday.scores
 
 def processScore(score):
   """
@@ -9,8 +9,8 @@ def processScore(score):
   # NOTE: This (wrongly) always seems to get "4/4".
   sig = score.getTimeSignatures()[0]
   notes = score.flat.pitches
-  intervals = thursday.music.makeNotesIntoTonicIntervals(notes, key.getTonic())
-  freq = thursday.music.sortIntervalFrequencies(thursday.music.getIntervalFrequencies(intervals))
+  intervals = tday.music.makeNotesIntoTonicIntervals(notes, key.getTonic())
+  freq = tday.music.sortIntervalFrequencies(tday.music.getIntervalFrequencies(intervals))
 
   print '# Score: ' + str(getattr(score, 'corpusFilepath', score.filePath))
   print '- Key of'
@@ -20,14 +20,14 @@ def processScore(score):
   print '  ' + str(sig.ratioString)
   print
   print '- Interval frequencies'
-  print '  ' + thursday.music.intervalFrequenciesToString(freq)
+  print '  ' + tday.music.intervalFrequenciesToString(freq)
   print
   print
 
 def main():
-  bachScores = thursday.scores.loadScores(thursday.scores.getComposerSources('bach', limit=2))
+  bachScores = tday.scores.loadScores(tday.scores.getComposerSources('bach', limit=2))
   [processScore(score) for score in bachScores]
-  beethovenScores = thursday.scores.loadScores(thursday.scores.getComposerSources('beethoven', limit=2))
+  beethovenScores = tday.scores.loadScores(tday.scores.getComposerSources('beethoven', limit=2))
   [processScore(score) for score in beethovenScores]
 
 if __name__ == '__main__':
